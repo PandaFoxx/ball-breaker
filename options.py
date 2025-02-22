da801e93d7573e90c9c43d74c9ca1c15c88ced37
Kyle Champion <kyleischampion@gmail.com>
2025-02-22 22:18:20 +0200

    Optimise options

--- options.py
+++ options.py
@@ -5,14 +5,14 @@ class options:
         self.screen_width = 800
         self.screen_height = 600
 
-        self.player_color = (0,255,0)
-        self.player_width = 200
+        self.player_color = (0,0,255)
+        self.player_width = 100
         self.player_height = 15
         self.player_x = (self.screen_width / 2) - (self.player_width / 2)
         self.player_y = self.screen_height - (self.player_height * 2)
         self.player_speed = 5
 
-        self.ball_color = (0,0,255)
+        self.ball_color = (0,255,0)
         self.ball_radius = 10
         self.ball_speed_x = 3
         self.ball_speed_y = 3
