429c851efba0adb2bb88ba25fb1e8c1016ececa0
Kyle Champion <kyleischampion@gmail.com>
2025-02-22 17:19:01 +0200

    Move paddle class to another file

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+from paddle import paddle
 
 pygame.init()
 
@@ -10,28 +11,6 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
 pygame.display.set_caption("Breakout")
-
-class paddle:
-    def __init__(self, boundary_w, boundary_h):
-        self.boundary_w = boundary_w-2
-        self.boundary_h = boundary_h
-        self.width = 200
-        self.height = 15
-        self.colour = (0,255,0)
-        self.x = (boundary_w / 2) - (self.width / 2)
-        self.y = boundary_h - (self.height * 2)
-        self.speed = 1
-
-    def rect(self):
-        return (self.x, self.y, self.width, self.height)
-    
-    def move_left(self):
-        if self.x > 2:
-            self.x -= self.speed
-
-    def move_right(self):
-        if (self.x + self.width) < self.boundary_w:
-            self.x += self.speed
 
 player = paddle(SCREEN_WIDTH, SCREEN_HEIGHT)
 
---
+++ paddle.py
@@ -0,0 +1,21 @@
+class paddle:
+    def __init__(self, boundary_w, boundary_h):
+        self.boundary_w = boundary_w-2
+        self.boundary_h = boundary_h
+        self.width = 200
+        self.height = 15
+        self.colour = (0,255,0)
+        self.x = (boundary_w / 2) - (self.width / 2)
+        self.y = boundary_h - (self.height * 2)
+        self.speed = 1
+
+    def rect(self):
+        return (self.x, self.y, self.width, self.height)
+    
+    def move_left(self):
+        if self.x > 2:
+            self.x -= self.speed
+
+    def move_right(self):
+        if (self.x + self.width) < self.boundary_w:
+            self.x += self.speed
