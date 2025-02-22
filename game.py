a77304e1f87686ac5f8358cc471521e58b444967
Kyle Champion <kyleischampion@gmail.com>
2025-02-22 15:51:43 +0200

    Initialize base game

---
+++ game.py
@@ -0,0 +1,23 @@
+import pygame
+import sys
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+
+pygame.display.set_caption("Breakout")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit()
